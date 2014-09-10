57753f2baaf7f4b5270bffe246fb707a7c7979d8
verena91 <vereojeda@gmail.com>
2014-09-10 17:22:16 -0400

    Cambio en el manejo de archivos. Se crean directorios para el data.json de cada domain. Se eliminar archivos temporales.

---
+++ crawler/file_controller.py
@@ -0,0 +1,47 @@
+__author__ = 'Verena Ojeda'
+
+import codecs
+import os
+import time
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+class FileController:
+    def clean_tmp_files(self):
+        """
+        Elimina los archivos temporales utilizados por el spider.
+        """
+        if os.path.exists("aux.html"):
+            os.remove("aux.html")
+        if os.path.exists("splash.html"):
+            os.remove("splash.html")
+
+
+    def clean_item_tmp_file(self, domain):
+        """
+        Elimina los archivos temporales utilizados por el spider.
+        """
+        file =  domain + ".json"
+        if os.path.exists(file):
+             os.remove(file)
+
+
+    def save_existing_data_json(self, response, domain, to_json):
+        """
+        Guarda un data.json nuevo y/o existente.
+        """
+        principal = "results_" + time.strftime("%d_%m_%y")
+        if not os.path.exists(principal):
+            os.makedirs(principal)
+        subprincipal = principal + "/" + domain
+        if not os.path.exists(subprincipal):
+            os.makedirs(subprincipal)
+        final = subprincipal + "/" + "data.json"
+        file_response = codecs.open(final, 'w+', 'utf-8-sig')
+        if to_json == True:
+            file_response.write(json.dumps(response.json(), indent=2, ensure_ascii=False))
+        else:
+            file_response.write(json.dumps(response, indent=2, ensure_ascii=False))
